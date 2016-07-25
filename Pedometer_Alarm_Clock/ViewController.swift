9637fda9d87353f87646103d1740cca5bdca7c6a
bhardwajaakarshan <bhardwaj.aakarshan@gmail.com>
2016-07-24 20:16:09 -0400

    Initial Commit

---
+++ Pedometer_Alarm_Clock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pedometer_Alarm_Clock
+//
+//  Created by Aash Bhardwaj on 7/24/16.
+//  Copyright © 2016 Aash Bhardwaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pedometer_Alarm_ClockTests/Pedometer_Alarm_ClockTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pedometer_Alarm_ClockTests.swift
+//  Pedometer_Alarm_ClockTests
+//
+//  Created by Aash Bhardwaj on 7/24/16.
+//  Copyright © 2016 Aash Bhardwaj. All rights reserved.
+//
+
+import XCTest
+@testable import Pedometer_Alarm_Clock
+
+class Pedometer_Alarm_ClockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
