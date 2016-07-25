09b285db77d4094af845751d8150baf6bec9ede4
bhardwajaakarshan <bhardwaj.aakarshan@gmail.com>
2016-07-25 18:15:14 -0400

    Ability to access steps at a faster rate

--- Pedometer_Alarm_Clock/ViewController.swift
+++ Pedometer_Alarm_Clock/ViewController.swift
@@ -12,6 +12,7 @@ import CoreMotion
 class ViewController: UIViewController {
     
     var currSteps = 0
+    let totalSteps = 100
     var pedEvent = CMPedometer()
     var date = NSDate()
     let lengthFormatter = NSLengthFormatter()
@@ -20,30 +21,71 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        stepDisplay.text! = String(currSteps)
-        print("LOL")
+        stepDisplay.text! = String(totalSteps - currSteps)
+        registerLocal(self)
+        date = NSDate()
+        if(CMPedometer.isStepCountingAvailable()){
+            
+            pedEvent.startPedometerUpdatesFromDate(NSDate(), withHandler: { data, error in
+                print(data!.numberOfSteps)
+                self.currSteps = data!.numberOfSteps.integerValue
+                self.stepDisplay.text! = String(self.totalSteps - self.currSteps)
+                
+            })
+        }
     }
+    
+    @IBAction func trigger(sender: UIButton) {
+        startCounting()
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func resetSteps(sender: UIButton) {
-
+    func registerLocal(sender: AnyObject) {
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+    }
+    
+    func scheduleLocal(sender: AnyObject) {
+        let notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 5)
+        notification.soundName = UILocalNotificationDefaultSoundName
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func resetSteps(sender: AnyObject) {
+        // localdata is used to access the information outside of the closure
         if(CMPedometer.isStepCountingAvailable()){
-            print("WE HAVE PERMISSION")
-            
-            self.pedEvent.startPedometerUpdatesFromDate(NSDate(), withHandler: { data, error in
-                print(data!.numberOfSteps)
-            })
-
+            var localdata:CMPedometerData!
             self.pedEvent.queryPedometerDataFromDate(date, toDate: NSDate(), withHandler: { data, error in
-                print(data!.numberOfSteps)
+                print("\(data!.numberOfSteps) ASDF1")
+                localdata = data!
             })
+            sleep(1)
+            // since a second thread is created sleep is used to make sure the local value is not dereferenced before the app returns the number of steps.
+            print("\(localdata.numberOfSteps)")
+            currSteps = (localdata!.numberOfSteps).integerValue
+            stepDisplay.text! = String(localdata!.numberOfSteps.integerValue)
+        } else {
+            print("No permission to use health data.")
         }
         
         
+    }
+    
+    func startCounting () {
+        var currdate = NSDate()
+        while (NSDate().timeIntervalSinceDate(currdate) < 2) {}
+        currdate = NSDate()
+        resetSteps(self)
+        stepDisplay.text! = String(totalSteps - currSteps)
+        if (totalSteps - currSteps > 0) {
+            startCounting()
+        }
     }
     
 }
